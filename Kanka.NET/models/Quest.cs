c08e7593cc35e1b50f03171d575e729f3b425f2d
Andrew Schlachter <DocSchlock@users.noreply.github.com>
2022-01-12 11:23:00 -0500

    add Quest

---
+++ Kanka.NET/models/Quest.cs
@@ -0,0 +1,59 @@
+﻿using Kanka.NET.models.interfaces;
+using System.Text.Json.Serialization;
+
+namespace Kanka.NET.models
+{
+    public class Quest : EndpointAbstract, ICampaignRequired
+    {
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("is_completed")]
+        public bool IsCompleted { get; set; }
+
+        [JsonPropertyName("quest_id")]
+        public int? QuestId { get; set; }
+
+        [JsonPropertyName("elements")]
+        public QuestElement[] Elements { get; set; }
+    }
+
+    public class QuestElement
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("created_by")]
+        public string? CreatedBy { get; set; }
+
+        [JsonPropertyName("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonPropertyName("updated_by")]
+        public string? UpdatedBy { get; set; }
+
+        [JsonPropertyName("entity_id")]
+        public int EntityId { get; set; }
+
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("description_parsed")]
+        public string DescriptionParsed { get; set; }
+
+        [JsonPropertyName("visibility")]
+        public string Visibility { get; set; }
+    }
+}
