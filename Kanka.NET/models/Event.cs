4ec1cbb71f85c955b22b15b7f82b149e50e862a7
Andrew Schlachter <DocSchlock@users.noreply.github.com>
2022-01-12 11:09:11 -0500

    Add Event;

---
+++ Kanka.NET/models/Event.cs
@@ -0,0 +1,14 @@
+﻿using Kanka.NET.models.interfaces;
+using System.Text.Json.Serialization;
+
+namespace Kanka.NET.models
+{
+    public class Event : EndpointAbstract, ICampaignRequired
+    {
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
