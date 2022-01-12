0c6038fb9d9507507ab4fed9a25fcd359cc28154
Andrew Schlachter <DocSchlock@users.noreply.github.com>
2022-01-12 11:07:35 -0500

    Add Note;

---
+++ Kanka.NET/models/Note.cs
@@ -0,0 +1,17 @@
+﻿using Kanka.NET.models.interfaces;
+using System.Text.Json.Serialization;
+
+namespace Kanka.NET.models
+{
+    public class Note : EndpointAbstract, ICampaignRequired
+    {
+        [JsonPropertyName("is_pinned")]
+        public int IsPinned { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("note_id")]
+        public int? NoteId { get; set; }
+    }
+}
